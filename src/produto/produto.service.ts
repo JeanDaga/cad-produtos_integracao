0d363ca08f7439dd9effaf40047d33a84589b4ac
Jean <jsdaga@minha.fag.edu.br>
2025-03-07 20:18:48 -0300

    ver 2.0

--- src/produto/produto.service.ts
+++ src/produto/produto.service.ts
@@ -1,19 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProdutoDto } from './dto/create-produto.dto';
 import { UpdateProdutoDto } from './dto/update-produto.dto';
+import { Produto } from './entities/produto.entity';
 
 @Injectable()
 export class ProdutoService {
+
+private produtos : Produto[] = [
+  {
+    id:1,
+    nome:'RTX 7090Ti super ProMax',
+    preco: 30000,
+    qtdStock: 4,
+    categoria:'Placa de Video'
+  },
+  {
+    id:2,
+    nome:'mouse',
+    preco:50,
+    qtdStock: 45,
+    categoria:'Perefericos'
+  }
+];
+
   create(createProdutoDto: CreateProdutoDto) {
     return 'This action adds a new produto';
   }
 
   findAll() {
-    return `This action returns all produto`;
+    return this.produtos;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} produto`;
+  findOne(id: number): Produto | undefined {
+      return this.produtos.find(produto => produto.id === id);
+    
   }
 
   update(id: number, updateProdutoDto: UpdateProdutoDto) {
